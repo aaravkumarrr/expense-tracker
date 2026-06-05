c6c8cc8bd94731c312ec9dca5c6d30f91062d7bf
AK <aarav1102006@gmail.com>
2026-06-05 10:54:02 +0800

    Update expense-tracker.py
    Updated the tracker again.

--- expense-tracker.py
+++ expense-tracker.py
@@ -1,8 +1,40 @@
+import json 
+import os
+
+# finds the name of the folder the file is in
+script_dir = os.path.dirname(os.path.abspath(__file__))
+# finds the path of the file by joining the folderpath with the filename
+file_path = os.path.join(script_dir, "expenses_file.txt")
 
 while True:
-    decision = input("What would you like to do?\n    1: Add expense\n    2: View expenses\n    3: Exit\n")
+
+    decision = int(input("What would you like to do?\n    1: Add expense\n    2: View expenses\n    3: Exit\n\n"))
 
     if (decision == 1):
-        expense_profile = {}
-        with open("expenses_file.txt","a") as exp:
-            json.dump()
+
+        item = input("What did you purchase?\n")
+        cost = float(input("How much did it cost?\n"))
+
+        try:
+            with open(file_path,"r") as exp:
+                expenses = json.load(exp)
+        except FileNotFoundError:
+            expenses = []
+        
+        expenses.append({"expense":item, "cost":cost})
+
+        with open(file_path, "w") as exp:
+            json.dump(expenses, exp)
+    
+    elif (decision == 2):
+        try:
+            with open(file_path,"r") as exp:
+                expenses = json.load(exp)
+        except FileNotFoundError:
+            expenses = []
+        
+        print("----------+----------")
+        print(f"item      |cost      ")
+        print("----------+----------")
+        for log in expenses:
+            print(f"{log["expense"]:<10}|{log["cost"]:<10}")
